68469dea2048bdb9880ddb5942459cb099cf2fb5
Manches <Lukjanovilly@gmail.com>
2025-04-03 00:41:30 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./js/header";
+import "./js/hero";
+import "./js/about-me";
+import "./js/project";
+import "./js/faq";
+import "./js/covers";
+import "./js/reviews";
+import "./js/work-together";
